61559f59574e9b70daf540512b0fb546f11e01c6
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-03-23 08:11:04 +0000

    commit

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const TwitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  if (!quote.author) {
+    authorText.textContent = 'Jon Doe';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {
